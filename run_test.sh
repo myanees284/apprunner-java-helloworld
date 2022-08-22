d91bc68ddb3621040388800efe25edda26de29d1
EC2 Default User <ec2-user@ip-13-0-0-193.us-east-2.compute.internal>
2022-08-22 09:19:24 +0000

    added test

---
+++ run_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+mvn clean package
+java -jar target/demo-0.0.1-SNAPSHOT.jar -Xms256m
+rName=$(date +%F)_$(date +%s)
+echo $rName
+echo "*******************Downloading JMeter*******************"
+curl -L --silent https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-5.5.tgz > /tmp/apache-jmeter-5.5.tgz
+yum -y install tar
+tar -xzf /tmp/apache-jmeter-5.5.tgz -C /opt
+export JMETER_HOME=/opt/apache-jmeter-5.5
+export JMETER_BIN=${JMETER_HOME}/bin
+export PATH=$PATH:$JMETER_BIN
+echo "*******************Starting load test*******************"
+jmeter -n -t $1.jmx -l $1_result.csv -e -o $1
+echo "*******************Test Complete*******************"
+mv $1_result.csv $1/
+yum install awscli -y
+echo "*******************Copying Results into S3 bucket*******************"
+aws s3 cp $1/ s3://loveisair/$1-$rName-html-report --recursive
